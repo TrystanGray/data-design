eddabb8fd023f756bca95f0dddcb15bab9741a70
Trystan Gray <trystangray7@gmail.com>
2019-04-09 20:14:19 -0600

    finished up user persona

---
+++ persona.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<h2>Persona</h2>
+		<img src="datadesign.jpg" alt="no pic">
+		<ul>
+			<li>Name: Mark Davis</li>
+			<li>Gender: Male</li>
+			<li>Age: 25</li>
+			<li>Technology
+			<ul>
+				<li>Type: Computer</li>
+				<li>Proficiency: 7</li>
+				<li>Loves working with computers.</li>
+			</ul>
+			<li>Attitudes and Needs
+			<ul>
+				<li>Why are they using the site? Trying to make a saved car list of cars he likes.</li>
+				<li>The site cargurus offers this service by creating a compilation of liked posts.</li>
+				<li>He likes cargurus because of the variety of imformation it offers about said car.</li>
+			</ul></li>
+			<li>Frustrations: None with particular site</li>
+			<li>Role: Power/ casual user</li>
+		</ul>
+		<h4><a href="./user-story.php">User Story</a></h4>
+	</body>
+</html>
